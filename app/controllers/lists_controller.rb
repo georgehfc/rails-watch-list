64cabc6e1a841160ea4eb2212fff990f41d03744
George Chaves <george_hfc@yahoo.com.br>
2022-07-14 01:06:09 +0100

    still missing movies and front

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,47 @@
+class ListsController < ApplicationController
+  # Filter
+  before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to list_path(@list)
+  end
+
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list.destroy
+
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end

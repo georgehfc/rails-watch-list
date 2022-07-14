12cec33b4435811d587b9491af794c47bdee6465
George Chaves <george_hfc@yahoo.com.br>
2022-07-14 01:09:46 +0100

    rake completed though

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,11 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     @list.save
 
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def edit

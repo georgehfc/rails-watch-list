24f2cfb88bdde54263e10dadd98581ee04f1e7b3
George Chaves <george_hfc@yahoo.com.br>
2022-07-14 01:46:30 +0100

    still missing movies and front

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists do
-    resources :bookmarks, only: [ :new, :create ]
+    resources :bookmarks, only: [:new, :create]
+    resources :movies, only: [:show]
   end
-  resources :bookmarks, only: [ :destroy ]
+  resources :bookmarks, only: [:destroy]
 end
